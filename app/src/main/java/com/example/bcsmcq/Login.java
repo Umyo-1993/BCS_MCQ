d62fea49700f1b2b0d1793d30606c60d349d91c5
umotheing <umotheing1993@gmail.com>
2021-02-20 15:06:25 +0600

    remember me and forget password added

--- app/src/main/java/com/example/bcsmcq/Login.java
+++ app/src/main/java/com/example/bcsmcq/Login.java
@@ -3,12 +3,14 @@ package com.example.bcsmcq;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,6 +25,12 @@ public class Login extends AppCompatActivity {
     TextView returnsignup;
     Button login;
     private FirebaseAuth mAuth;
+    CheckBox rememberme;
+    TextView forgetpw;
+
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +43,27 @@ public class Login extends AppCompatActivity {
         passworded = findViewById(R.id.selectedpw);
         login = findViewById(R.id.signin);
         returnsignup=findViewById(R.id.textbottom2);
+        rememberme=findViewById(R.id.checkbox);
+        sharedPreferences=getSharedPreferences("LoginPrefs", MODE_PRIVATE);
+        editor=sharedPreferences.edit();
         String email, password;
         email = emailed.getText().toString();
         password = passworded.getText().toString();
+        forgetpw=findViewById(R.id.forgetpassword);
+
         //return to Registeractivity
+        if(email.equals(""))
+        {
+            emailed.setText(email.toString());
+        }
+        if(password.equals(""))
+        {
+           passworded.setText(password.toString());
+
+        }
+        emailed.setText(sharedPreferences.getString("email",""));
+        passworded.setText(sharedPreferences.getString("password",""));
+
         returnsignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,6 +79,29 @@ public class Login extends AppCompatActivity {
                 loginUserAccount();
             }
         });
+        forgetpw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailf = emailed.getText().toString();
+
+                if (TextUtils.isEmpty(emailf)) {
+                    Toast.makeText(getApplicationContext(), "Enter your email!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.sendPasswordResetEmail(emailf)
+                        .addOnCompleteListener(new OnCompleteListener() {
+                            @Override
+                            public void onComplete(@NonNull Task task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Login.this, "Check email to reset your password!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(Login.this, "Fail to send reset password email!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+            }
+        });
     }
 
     private void loginUserAccount()
@@ -64,6 +112,7 @@ public class Login extends AppCompatActivity {
 
         // Take the value of two edit texts in Strings
         String email, password;
+
         email = emailed.getText().toString();
         password = passworded.getText().toString();
 
@@ -99,7 +148,17 @@ public class Login extends AppCompatActivity {
                             Intent intent=new Intent(Login.this,StartPageActivity1.class);
                             startActivity(intent);
 
+                            if(rememberme.isChecked() ){
+                                Toast.makeText(Login.this, "Checked", Toast.LENGTH_SHORT).show();
+                                editor.putString("email",email);
+                                editor.putString("password",password);
 
+                                editor.commit();
+
+
+                            }else{
+                                Toast.makeText(Login.this, "Not checked...", Toast.LENGTH_SHORT).show();
+                            }
                             // if sign-in is successful
                             // intent to home activity
 
@@ -125,4 +184,6 @@ public class Login extends AppCompatActivity {
 
 
     }
+
+
 }
